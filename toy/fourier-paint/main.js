12760499dbf81c132dc0825c6ffe5d862361ebf5
co0okie <cookiequp3@gmail.com>
2025-07-16 15:33:45 +0800

    remove unused code, improve catmullRomCoef evaluation

--- toy/fourier-paint/main.js
+++ toy/fourier-paint/main.js
@@ -21,48 +21,6 @@ function linspace(a, b, n) {
     return x;
 }
 
-function range(a, b) {
-    const l = b - a + 1, x = Array(l);
-    for (let i = 0; i < l; i++) {
-        x[i] = a + i;
-    }
-    return x;
-}
-
-function compare(x, y) {
-    return x.reduce((a, b, i) => a + Math.abs(y[i] - b), 0);
-}
-
-/**
- * (t1, x1), (t2, x2)
- * (t - t1)(x2 - x1) = (x - x1)(t2 - t1)
- * x = (t - t1)(x2 - x1) / (t2 - t1) + x1
- * @param {number[]} x length l
- * @param {number[]} y length l
- * @param {number[]} t length l
- * @param {number} n interpolation count
- * @returns {[number[], number[], number[]]} [xx, yy, tt]: length n
- */
-function linearInterpolateN(x, y, t, n) {
-    const l = t.length;
-    const xx = Array(n), yy = Array(n), tt = linspace(t[0], t[l - 1], n);
-    let ti = 0, t1 = t[ti], t2 = t[ti + 1];
-    if (t2 === 0) t2 = (t1 + t[ti + 2]) / 2;
-    let x1 = x[ti], x2 = x[ti + 1], mx = (x2 - x1) / (t2 - t1);
-    let y1 = y[ti], y2 = y[ti + 1], my = (y2 - y1) / (t2 - t1);
-    for (let i = 0; i < n; i++) {
-        while (tt[i] > t2) {
-            ti++;
-            t1 = t2, t2 = t[ti + 1], x1 = x2, x2 = x[ti + 1], y1 = y2, y2 = y[ti + 1];
-            mx = (x2 - x1) / (t2 - t1);
-            my = (y2 - y1) / (t2 - t1);
-        }
-        xx[i] = (tt[i] - t1) * mx + x1;
-        yy[i] = (tt[i] - t1) * my + y1;
-    }
-    return [xx, yy, tt];
-}
-
 /**
  * 
  * @param {number} t20 - t2 - t0
@@ -72,15 +30,15 @@ function linearInterpolateN(x, y, t, n) {
  * @returns {[a0, a1, a2, a3]} - p(t) = a0 + a1*t + a2*t² + a3*t³, 0 <= t <= t21
  */
 function catmullRomCoef(t20, t21, t31, [p0, p1, p2, p3]) {
-    const denominatorA2 = t20 * t21 * t21 * t31;
-    const denominatorA3 = denominatorA2 * t21;
-    const common = t20 * t21 * (p1 - p3);
+    const m20 = (p2 - p0) / t20;
+    const m21 = (p2 - p1) / t21;
+    const m31 = (p3 - p1) / t31;
     
     return [
         p1,
-        (p2 - p0) / t20,
-        (common + 2*t21*t31*(p0 - p2) + 3*t20*t31*(p2 - p1)) / denominatorA2,
-        -(common + t21*t31*(p0 - p2) + 2*t20*t31*(p2 - p1)) / denominatorA3
+        m20,
+        (-2 * m20 - m31 + 3 * m21) / t21,
+        (m21 + m31 - 2 * m21) / t21 / t21
     ];
 }
 
@@ -142,119 +100,6 @@ function cartesian2Polar(x, y) {
         θ[i] = Math.atan2(y[i], x[i]);
     }
     return {r: r, θ: θ};
-}
-
-/**
- * y = x[0 ~ n - 2] + x[1 ~ n - 1]
- * @param {number[]} x length n
- * @returns {number[]} length n - 1
- */
-function mid2(x) {
-    const l = x.length - 1;
-    const y = Array(l);
-    for (let i = 0; i < l; i++) {
-        y[i] = x[i] + x[i + 1];
-    }
-    return y;
-}
-
-/**
- * y = x[1 ~ n - 1] - x[0 ~ n - 2]
- * @param {number[]} x length n
- * @returns {number[]} length n - 1
- */
-function diff(x) {
-    const l = x.length - 1;
-    const y = Array(l);
-    for (let i = 0; i < l; i++) {
-        y[i] = x[i + 1] - x[i];
-    }
-    return y;
-}
-
-/**
- * Trapezoidal Integration ∫ y dx
- * 
- * @param {number[]} x length n
- * @param {number[]} y length n
- * @returns {number}
- */
-function integral(x, y) {
-    const midy = mid2(y), dx = diff(x);
-    let sum = 0;
-    for (let i = 0, l = dx.length; i < l; i++) {
-        sum += midy[i] * dx[i];
-    }
-    return sum / 2;
-}
-
-/**
- * T = t[nt - 1] - t[0]
- * f = 1 / T
- * 
- * Z[n] = 1/T * ∫z(t) * exp(-j2πnft) dt
- *      = 1/T * ∫(x(t) + jy(t)) * (cos(-2πnft) + jsin(-2πnft)) dt
- *      = 1/T * ∫(x(t) + jy(t)) * (cos(2πnft) - jsin(2πnft)) dt
- *      = 1/T * ∫(x(t) + jy(t)) * (cos(2πnft) - jsin(2πnft)) dt
- *      = 1/T * ∫((x(t)cos(2πnft) + y(t)sin(2πnft)) + j(y(t)cos(2πnft) - x(t)sin(2πnft))) dt
- *      = 1/T * ∫(x(t)cos(2πnft) + y(t)sin(2πnft)) dt + j/T * ∫(y(t)cos(2πnft) - x(t)sin(2πnft)) dt
- *      = 1/T * ∫ X_(t) dt + 1/T * j∫ Y_(t) dt
- *      = X[n] + jY[n]
- * 
- * @param {number[]} x length nt
- * @param {number[]} y length nt
- * @param {number[]} t length nt
- * @param {number[]} k length nk
- * @returns {[number[], number[], number[], number[]]} [X, Y, k, T]: Z[k] = X[k] + jY[k], f = k / T
- */
-function FS(x, y, t, k) {
-  const nt = t.length, nk = k.length, T = t[nt - 1] - t[0], f = 1 / T;
-  const p2f = Math.PI * 2 * f;
-  const X = Array(nk).fill(0), Y = Array(nk).fill(0);
-  for (let ki = 0; ki < nk; ki++) {
-    const p2fn = p2f * k[ki];
-    const X_ = Array(nt), Y_ = Array(nt);
-    for (let ti = 0; ti < nt; ti++) {
-        const p2fnt = p2fn * t[ti];
-        const cp2fnt = Math.cos(p2fnt);
-        const sp2fnt = Math.sin(p2fnt);
-        X_[ti] = x[ti] * cp2fnt + y[ti] * sp2fnt;
-        Y_[ti] = y[ti] * cp2fnt - x[ti] * sp2fnt;
-    }
-    X[ki] = integral(t, X_) / T;
-    Y[ki] = integral(t, Y_) / T;
-  }
-  
-  return [X, Y, k, T];
-}
-
-/**
- * z(t) = ∑(Z[n] * exp(j2πnft))
- *      = ∑((X[n] + jY[n]) * (cos(2πnft) + jsin(2πnft)))
- *      = ∑(X[n]cos(2πnft) - Y[n]sin(2πnft)) + j∑(X[n]sin(2πnft) + Y[n]cos(2πnft))
- *      = x(t) + jy(t)
- * 
- * @param {number[]} X length nn
- * @param {number[]} Y length nn
- * @param {number[]} n length nn
- * @param {number} T
- * @param {number[]} t length nt
- * @returns {[number[], number[], number[]]} [x, y, t]: z(t) = x(t) + jy(t)
- */
-function IFS(X, Y, n, T, t) {
-    const nn = n.length, nt = t.length, f = 1 / T;
-    const p2f = Math.PI * 2 * f;
-    const x = Array(nt).fill(0), y = Array(nt).fill(0);
-    for (let ti = 0; ti < nt; ti++) {
-        const p2ft = p2f * t[ti];
-        for (let ni = 0; ni < nn; ni++) {
-            const cp2ftn = Math.cos(p2ft * n[ni]);
-            const sp2ftn = Math.sin(p2ft * n[ni]);
-            x[ti] += X[ni] * cp2ftn - Y[ni] * sp2ftn;
-            y[ti] += X[ni] * sp2ftn + Y[ni] * cp2ftn;
-        }
-    }
-    return [x, y, t];
 }
 
 /**
